1b41d6899b256ac43a4366cb5dde64cc579c1cb0
Abcdavid00 <83356700+Abcdavid00@users.noreply.github.com>
2023-05-25 15:20:31 +0700

    Clear everything

--- MOBIKE/src/assets/fonts/index.ts
+++
@@ -1,7 +0,0 @@
-export const POPPINS_LIGHT = 'Poppins-Light'
-export const POPPINS_LIGHT_ITALIC = 'Poppins-LightItalic'
-
-export const POPPINS_BOLD = 'Poppins-Bold'
-export const POPPINS_SEMI_BOLD = 'Poppins-SemiBold'
-
-export const POPPINS_REGULAR = 'Poppins-Regular'
--- MOBIKE/src/screens/Marketplace/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import MarketplaceComponent from '../../components/Marketplace';
-const Marketplace = ({navigation}) => {
-  return <MarketplaceComponent />;
-};
-
-export default Marketplace;
